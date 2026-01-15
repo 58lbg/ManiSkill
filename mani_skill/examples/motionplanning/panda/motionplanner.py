f5f2ce1dd4149eb407d49c83a0b9d80a3ac44336
zhangke05 <zhangke05@58.com>
2026-01-15 15:32:39 +0800

    尝试加入模型

--- mani_skill/examples/motionplanning/panda/motionplanner.py
+++ mani_skill/examples/motionplanning/panda/motionplanner.py
@@ -7,8 +7,8 @@ from mani_skill.examples.motionplanning.two_finger_gripper.motionplanner import 
 
 
 class PandaArmMotionPlanningSolver(TwoFingerGripperMotionPlanningSolver):
-    OPEN = 0.6
-    CLOSED = -0.6
+    OPEN = 1
+    CLOSED = -1
     MOVE_GROUP = "panda_hand_tcp"
 
     def __init__(
